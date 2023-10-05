9830d742ce88d561a4a069f80d464a9b66a21d19
rubywz <143305874+rubywz@users.noreply.github.com>
2023-10-05 20:00:47 +0700

    Update mahasiswa.php

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mahasiswa extends CI_Controller {
     public function index()
     {
-        $data['nama']='Annisa Mukhri';
-        $data['nim']='181401104';
+        $data['nama']='Annisa ';
+        $data['nim']='-';
         $data['kom']='B';
         $this->load->view('mahasiswa_view',$data);
     }
